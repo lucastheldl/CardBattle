2b3e51bf41746e3ae00d56c92f13e0000026fa82
lucastheldl <lucastheldl@gmail.com>
2023-09-26 01:30:04 -0300

    feat: added styles to header

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -6,8 +6,21 @@ export const HeaderContainer = styled.nav`
 
   padding: 2rem;
 
+  font-size: 1.5rem;
+  font-weight: bold;
+
+  border-bottom: 2px solid ${(props) => props.theme["gray-800"]};
+  margin-bottom: 2rem;
+
   ul {
     display: flex;
     gap: 1rem;
+
+    list-style: none;
+  }
+
+  a {
+    text-decoration: none;
+    color: ${(props) => props.theme["gray-800"]};
   }
 `;
