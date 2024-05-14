15df1dd337fc0257f504a704dedd6dbdc7f6c10c
lucastheldl <lucastheldl@gmail.com>
2024-05-14 19:01:26 -0300

    styles: improved the display of card moves in createCard

--- src/pages/CreateCard/index.tsx
+++ src/pages/CreateCard/index.tsx
@@ -1,14 +1,13 @@
 import { useState, FormEvent, ChangeEvent } from "react";
 
-import {
-  RegisterWrapper,
-  RegisterForm,
-  RegisterInput,
-} from "../auth/Register/styles";
+import { RegisterForm, RegisterInput } from "../auth/Register/styles";
 import { addDoc, collection } from "firebase/firestore";
 import { db } from "../../firebase/config";
 import { DefaultBtn } from "../../styles/global";
 import {
+  CreateCardWrapper,
+  FormContainer,
+  MovesContainer,
   RarityBtnContainer,
   RaritySection,
   RarityTextContainer,
@@ -72,136 +71,143 @@ export function CreateCard() {
     setMoves((prevstate) => prevstate.filter((m) => m.name != name));
   }
   return (
-    <RegisterWrapper>
-      {moves.map((m) => {
-        return (
-          <div>
-            <p>{m.name}</p>
-            <button onClick={() => handleRemoveMove(m.name)}>Remove</button>
-          </div>
-        );
-      })}
-      <h2>Criar uma carta</h2>
-      <RegisterForm onSubmit={handleSubmit}>
-        <label>Nome da Carta:</label>
-        <RegisterInput
-          placeholder="Battle ox"
-          type="text"
-          onChange={(e) => setName(e.target.value)}
-          value={name}
-          name="name"
-          required
-        />
+    <CreateCardWrapper>
+      <FormContainer>
+        <h2>Criar uma carta</h2>
+        <RegisterForm onSubmit={handleSubmit}>
+          <label>Nome da Carta:</label>
+          <RegisterInput
+            placeholder="Battle ox"
+            type="text"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+            name="name"
+            required
+          />
 
-        <label htmlFor="imgUrl">Imagen URL:</label>
-        <RegisterInput
-          type="text"
-          placeholder="Insira uma URL"
-          onChange={(e) => setImgUrl(e.target.value)}
-          value={imgUrl}
-          name="imgUrl"
-          required
-        />
-        <label htmlFor="characterImgUrl">Imagen do personagem URL:</label>
-        <RegisterInput
-          type="text"
-          placeholder="Insira uma URL"
-          onChange={(e) => setCharacterImgUrl(e.target.value)}
-          value={characterImgUrl}
-          name="characterImgUrl"
-          required
-        />
-        <label htmlFor="atk">Atk:</label>
-        <RegisterInput
-          type="number"
-          placeholder="Digite um ataque"
-          onChange={(e) => setAtk(Number(e.target.value))}
-          value={atk}
-          name="atk"
-          required
-        />
-        <label htmlFor="def">Def:</label>
-        <RegisterInput
-          type="number"
-          placeholder="Digite uma defesa"
-          onChange={(e) => setDef(Number(e.target.value))}
-          value={def}
-          name="def"
-          required
-        />
-        <label htmlFor="def">Hp:</label>
-        <RegisterInput
-          type="number"
-          placeholder="Digite um HP"
-          onChange={(e) => setHp(Number(e.target.value))}
-          value={hp}
-          name="hp"
-          required
-        />
-        <h3>Raridade</h3>
-        <RaritySection>
-          <RarityTextContainer>
-            <label>Comun</label>
-            <label>Raro</label>
-            <label>Lendário</label>
-          </RarityTextContainer>
-          <RarityBtnContainer>
-            <RegisterInput
-              type="radio"
-              onChange={(e) => handleRadioChange(e)}
-              value={"COMMON"}
-              name="rarity-1"
-              checked={rarity === "COMMON"}
-            />
-            <RegisterInput
-              type="radio"
-              onChange={(e) => handleRadioChange(e)}
-              value={"RARE"}
-              name="rarity-2"
-              checked={rarity === "RARE"}
-            />
-            <RegisterInput
-              type="radio"
-              onChange={(e) => handleRadioChange(e)}
-              value={"LEGENDARY"}
-              name="rarity-3"
-              checked={rarity === "LEGENDARY"}
-            />
-          </RarityBtnContainer>
-        </RaritySection>
-        <h3>Move-1</h3>
+          <label htmlFor="imgUrl">Imagen URL:</label>
+          <RegisterInput
+            type="text"
+            placeholder="Insira uma URL"
+            onChange={(e) => setImgUrl(e.target.value)}
+            value={imgUrl}
+            name="imgUrl"
+            required
+          />
+          <label htmlFor="characterImgUrl">Imagen do personagem URL:</label>
+          <RegisterInput
+            type="text"
+            placeholder="Insira uma URL"
+            onChange={(e) => setCharacterImgUrl(e.target.value)}
+            value={characterImgUrl}
+            name="characterImgUrl"
+            required
+          />
+          <label htmlFor="atk">Atk:</label>
+          <RegisterInput
+            type="number"
+            placeholder="Digite um ataque"
+            onChange={(e) => setAtk(Number(e.target.value))}
+            value={atk}
+            name="atk"
+            required
+          />
+          <label htmlFor="def">Def:</label>
+          <RegisterInput
+            type="number"
+            placeholder="Digite uma defesa"
+            onChange={(e) => setDef(Number(e.target.value))}
+            value={def}
+            name="def"
+            required
+          />
+          <label htmlFor="def">Hp:</label>
+          <RegisterInput
+            type="number"
+            placeholder="Digite um HP"
+            onChange={(e) => setHp(Number(e.target.value))}
+            value={hp}
+            name="hp"
+            required
+          />
+          <h3>Raridade</h3>
+          <RaritySection>
+            <RarityTextContainer>
+              <label>Comun</label>
+              <label>Raro</label>
+              <label>Lendário</label>
+            </RarityTextContainer>
+            <RarityBtnContainer>
+              <RegisterInput
+                type="radio"
+                onChange={(e) => handleRadioChange(e)}
+                value={"COMMON"}
+                name="rarity-1"
+                checked={rarity === "COMMON"}
+              />
+              <RegisterInput
+                type="radio"
+                onChange={(e) => handleRadioChange(e)}
+                value={"RARE"}
+                name="rarity-2"
+                checked={rarity === "RARE"}
+              />
+              <RegisterInput
+                type="radio"
+                onChange={(e) => handleRadioChange(e)}
+                value={"LEGENDARY"}
+                name="rarity-3"
+                checked={rarity === "LEGENDARY"}
+              />
+            </RarityBtnContainer>
+          </RaritySection>
+          <h3>Move</h3>
 
-        <label>Name:</label>
-        <RegisterInput
-          type="text"
-          placeholder="Insira nome"
-          onChange={(e) => setMoveOneName(e.target.value)}
-          value={moveOneName}
-          name="move-1-name"
-          required
-        />
-        <label>Damage:</label>
-        <RegisterInput
-          type="number"
-          placeholder="Insira um dano"
-          onChange={(e) => setMoveOneDamage(Number(e.target.value))}
-          value={moveOneDamage}
-          name="move-1-damage"
-          required
-        />
-        <label>Cooldown:</label>
-        <RegisterInput
-          type="number"
-          placeholder="Insira um cooldown"
-          onChange={(e) => setMoveOneCooldown(Number(e.target.value))}
-          value={moveOneCooldown}
-          name="move-1-cooldown"
-          required
-        />
-        <DefaultBtn onClick={handleAddMove}>Add move</DefaultBtn>
+          <label>Name:</label>
+          <RegisterInput
+            type="text"
+            placeholder="Insira nome"
+            onChange={(e) => setMoveOneName(e.target.value)}
+            value={moveOneName}
+            name="move-1-name"
+            required
+          />
+          <label>Damage:</label>
+          <RegisterInput
+            type="number"
+            placeholder="Insira um dano"
+            onChange={(e) => setMoveOneDamage(Number(e.target.value))}
+            value={moveOneDamage}
+            name="move-1-damage"
+            required
+          />
+          <label>Cooldown:</label>
+          <RegisterInput
+            type="number"
+            placeholder="Insira um cooldown"
+            onChange={(e) => setMoveOneCooldown(Number(e.target.value))}
+            value={moveOneCooldown}
+            name="move-1-cooldown"
+            required
+          />
+          <DefaultBtn onClick={handleAddMove}>Add move</DefaultBtn>
 
-        <DefaultBtn type="submit">Criar carta</DefaultBtn>
-      </RegisterForm>
-    </RegisterWrapper>
+          <DefaultBtn type="submit">Criar carta</DefaultBtn>
+        </RegisterForm>
+      </FormContainer>
+
+      <MovesContainer>
+        {moves.map((m, i) => {
+          return (
+            <div key={i} className="moveSlot">
+              <strong>{m.name}</strong>
+              <p>Damage:{m.damage}</p>
+              <p>cooldown:{m.cooldown}</p>
+              <button onClick={() => handleRemoveMove(m.name)}>Remove</button>
+            </div>
+          );
+        })}
+      </MovesContainer>
+    </CreateCardWrapper>
   );
 }
--- src/pages/CreateCard/styles.ts
+++ src/pages/CreateCard/styles.ts
@@ -1,11 +1,42 @@
 import styled from "styled-components";
 
+export const CreateCardWrapper = styled.main`
+  display: flex;
+  justify-content: center;
+  gap: 1rem;
+  width: 100%;
+  max-width: 1120px;
+  margin: 0 auto;
+`;
+
+export const FormContainer = styled.div`
+  h3 {
+    color: ${(props) => props.theme["red-500"]};
+    padding-bottom: 2rem;
+  }
+`;
+export const MovesContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding: 1rem;
+
+  .moveSlot {
+    display: flex;
+    flex-direction: column;
+    width: 200px;
+    color: ${(props) => props.theme["red-500"]};
+    padding: 1rem;
+    border: 1px solid ${(props) => props.theme["red-500"]};
+    border-radius: 10px;
+  }
+`;
 export const RaritySection = styled.div`
   display: flex;
   align-items: center;
   gap: 5rem;
   padding-bottom: 2rem;
-
+  padding: 1rem;
   width: 30%;
 `;
 export const RarityBtnContainer = styled.div`
